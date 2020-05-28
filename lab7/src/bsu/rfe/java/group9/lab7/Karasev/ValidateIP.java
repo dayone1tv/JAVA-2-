e9cb2e1f976eb9414515af198eb34031e260ae3c
Kostya <tvdayone@gmail.com>
2020-05-28 18:50:28 +0300

    lab7 done

---
+++ lab7/src/bsu/rfe/java/group9/lab7/Karasev/ValidateIP.java
@@ -0,0 +1,16 @@
+package bsu.rfe.java.group9.lab7.Karasev;
+
+import java.net.Inet4Address;
+import java.net.Inet6Address;
+import java.net.UnknownHostException;
+
+class ValidateIP {
+    public static boolean isValidIP(String ip) {
+        try {
+            return !Inet4Address.getByName(ip)
+                    .getHostAddress().equals(ip);
+        } catch (UnknownHostException ex) {
+            return true;
+        }
+    }
+}
